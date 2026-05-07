31b7ba3ea0e91d975e3706a684819a69d96ee953
GPC Thushani <charithathushani2003@gmail.com>
2026-05-07 08:53:19 +0530

    Update API base URL to a specific IP address

--- mobile-app/src/services/api.js
+++ mobile-app/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-// This pulls the URL from the local .env file of whoever is running the code
-const apiUrl = process.env.EXPO_PUBLIC_API_URL || "http://localhost:5000/api";
-
 const API = axios.create({
-  baseURL: apiUrl,
-  timeout: 10000,
+  baseURL: "http://192.168.8.103:5000/api",
 });
 
 export default API;
